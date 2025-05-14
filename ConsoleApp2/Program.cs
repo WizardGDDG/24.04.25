1f65a4c35d08365fbd058f5ea6f9e7a0e17a4b8f
Колледж <@DESKTOP-L1CK0CT>
2025-05-14 17:39:57 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace MyNameSpace 
+{
+    static class MyClass
+    {
+        static int Rectangle(int A, int B, int C)
+        {
+            if (C > A | C > B)
+            {
+                Console.WriteLine("Ошибка");
+                return 0;
+            }
+            else
+            {
+                int sum = A * B;
+                return sum /= C;
+            }
+
+
+        }
+        static void Bank() 
+        {
+            int P;
+            int k = 0;
+            int s = 10000;
+            do
+            {
+                P = Convert.ToInt32(Console.ReadLine());
+            } while (P<0 | P > 25);
+
+            while (s < 11000) 
+            {
+                s += s / 100 * P;
+                k++;
+            }
+            
+            Console.WriteLine(s);
+            Console.WriteLine(k);
+        }
+
+        static void Main(string[] args) 
+        {
+            
+
+        }
+ 
+            
+    }
+}
+
+
